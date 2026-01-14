a6ff05d1111a96d1cf2339dcd83c9a963cfa3efb
Nico Corrales <nicocorrales13@gmail.com>
2026-01-14 12:15:04 +0100

    act 14.4 instalar en movil

--- sw.js
+++ sw.js
@@ -5,3 +5,30 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", () => {
     console.log("Service Worker activado");
 });
+
+const cacheName = 'mi-app-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js'
+];
+
+// Instalación
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+// Activación
+self.addEventListener('activate', e => {
+  console.log('Service Worker activado');
+});
+
+// Interceptar requests
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
