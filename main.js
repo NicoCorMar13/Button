11317b2d6f8e44925fb5f80d247450afa60f8d15
Nico Corrales <nicocorrales13@gmail.com>
2026-01-15 11:48:01 +0100

    eliminacion de codigo antiguo comentado

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-/*const button = document.getElementById("boton");
-
-console.log("JS cargado:", button);
-
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js")
-        .then(() => console.log("Service Worker registrado"))
-        .catch(err => console.error("Error SW:", err));
-}
-
-if (button) {
-    button.addEventListener("click", async () => {
-        console.log("Botón pulsado");
-
-        const permission = await Notification.requestPermission();
-        console.log("Permiso:", permission);
-
-        if (permission === "granted") {
-            const registration = await navigator.serviceWorker.ready;
-            registration.showNotification("¡Hola! 👋", {
-                body: "Has hecho clic en el botón",
-            });
-        } else {
-            alert("No diste permiso para notificaciones");
-        }
-    });
-} else {
-    console.error("❌ No se encontró el botón notifyBtn");
-}*/
-
 // Lista de días
 const dias = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
 const contenedor = document.getElementById("semanas");
